e734e4f8ae52b395b7d7957655a25847dd1adf1b
kalebunna <siconfix@gmail.com>
2023-02-01 19:28:15 +0700

    adding new arrival in lading page

--- app/Http/Livewire/LandingPage/NewArrival.php
+++ app/Http/Livewire/LandingPage/NewArrival.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\LandingPage;
 
+use App\Models\product;
 use Livewire\Component;
 
 class NewArrival extends Component
 {
     public function render()
     {
-        return view('livewire.landing-page.new-arrival');
+        $data = product::with('Category')->latest('id')->limit(10)->get();
+        return view('livewire.landing-page.new-arrival', compact('data'));
     }
 }
--- app/Models/product.php
+++ app/Models/product.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','foto','qrcode','deskripsi','harga','harga_promo','category_id','stok'];
+    protected $fillable = ['name', 'foto', 'qrcode', 'deskripsi', 'harga', 'harga_promo', 'category_id', 'stok'];
+
+    public function Category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
