7143ecc055d0444a6cc39a5e43e6647bcad339bf
kalebunna <siconfix@gmail.com>
2023-02-02 16:01:20 +0700

    landing page icon last update

--- app/Http/Livewire/LandingPage/ListProduct.php
+++ app/Http/Livewire/LandingPage/ListProduct.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\LandingPage;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class ListProduct extends Component
 {
     public function render()
     {
-        return view('livewire.landing-page.list-product');
+        $data = Category::with('products')->get();
+        // dd($data->Products);
+        // dd($data);
+        // dd($data);
+        return view('livewire.landing-page.list-product', compact('data'));
     }
 }
