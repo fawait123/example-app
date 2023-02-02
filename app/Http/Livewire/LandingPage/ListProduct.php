723a8a6c3c9a6ac8cfd5aa9b4cc7e0917bcd3a3d
kalebunna <siconfix@gmail.com>
2023-02-02 17:09:21 +0700

    resolving data list product in landing page

--- app/Http/Livewire/LandingPage/ListProduct.php
+++ app/Http/Livewire/LandingPage/ListProduct.php
@@ -10,9 +10,6 @@ class ListProduct extends Component
     public function render()
     {
         $data = Category::with('products')->get();
-        // dd($data->Products);
-        // dd($data);
-        // dd($data);
         return view('livewire.landing-page.list-product', compact('data'));
     }
 }
